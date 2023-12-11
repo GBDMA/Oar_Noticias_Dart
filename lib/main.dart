47f8c594f0656ddc17c554c866d11487fc7a41fb
Gabriel Bento <91234491+GBDMA@users.noreply.github.com>
2023-12-11 19:57:34 -0300

    Adicionando Consumo da API DE NOTICAS "NEWS API"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(NewsApp());
@@ -35,15 +37,12 @@ class HomePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // Texto "Acesse todas as notícias de hoje"
               Text(
                 'Acesse todas as notícias de hoje',
                 style: TextStyle(fontSize: 18.0),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(
-                  height: 20), // Adiciona um espaço entre o texto e o botão
-              // Botão "Acessar Notícias"
+              SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/newsPage');
@@ -65,33 +64,42 @@ class NewsHomePage extends StatefulWidget {
 
 class _NewsHomePageState extends State<NewsHomePage> {
   int _currentTabIndex = 0;
-  List<Map<String, String>> _newsContent = [
-    {
-      'title':
-          'Promotoria antiterrorista investiga assassinato de turista próximo à Torre Eiffel em Paris',
-      'content':
-          'A Promotoria Nacional Antiterrorista (Pnat) abriu uma investigação por assassinato e tentativa de assassinato relacionada a atividade terrorista e por associação criminosa terrorista. Outras três pessoas de seu círculo também foram detidas, informou a Pnat neste domingo.O promotor antiterrorismo Jean-François Ricard disse à imprensa que o agressor havia jurado lealdade ao grupo Estado Islâmico (EI) em um vídeo no início de outubro, onde, falando em árabe, expressou "apoio a jihadistas que operam em diferentes áreas".O ataque ocorreu por volta das 21h locais (17h em Brasília) perto da ponte Bir Hakeim, um lugar muito frequentado por turistas devido à sua proximidade com a Torre Eiffel.',
+  List<Map<String, dynamic>> _newsContent = [];
 
-      'image':
-          'https://s2-oglobo.glbimg.com/Kf31PTBMhQCqsh7s5ZmHgpoQjmk=/0x0:8256x5504/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/5/A/BoR9UJQdWf9X7cGlSUNg/105195127-police-officers-stand-guard-in-a-security-perimetre-set-after-one-person-was-killed-and-t.jpg', // Notícia 1
-    },
-    {
-      'title':
-          'Trecho da BR-040 em Minas registra uma morte a cada 2 km em 2023',
-      'content':
-          'Um trecho de 54 km da BR-040, entre o condomínio Alphaville, em Nova Lima, na Grande BH, e Congonhas, região Central de Minas, registrou 27 mortes em acidentes de dezembro de 2022 até agora. O dado faz parte uma uma prévia de um relatório feito pelo engenheiro civil Hérzio Mansur, integrante de um grupo de trabalho de segurança no trânsito do Conselho Federal de Engenharia e Agronomia (Confea).',
-      'image':
-          'https://www.hojeemdia.com.br/image/policy:1.730296.1628590779:1628590779/image.jpg?f=2x1&w=1000&', // Noticia 2
-    },
-    {
-      'title':
-          'Vídeo mostra ex-marido de Naiara Azevedo dando tapa em celular para parar gravação',
-      'content':
-          'Na gravação, Naiara e Rafael mostram alguns ferimentos nos braços. "Rafael está falando que eu agredi ele porque ele juntou a mão nas minhas duas orelhas. Olha os meus braços", diz ela. Rafael então questiona: "Quem me deu um murro na cara primeiro, Naiara?". A cantora responde: "Eu não te dei murro na cara. Você falou que minha mãe é uma puta".Nesse momento, Rafael diz para Naiara falar a verdade e bate no celular para parar a gravação.',
-      'image':
-          'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/12/04/fotos-(74)-t4idgnhfkaxv.jpg', // Notícia 3
-    },
-  ];
+  @override
+  void initState() {
+    super.initState();
+    fetchNews();
+  }
+
+  Future<void> fetchNews() async {
+    final apiKey = '0e390a35e2ac4f9f860a30aa1c75938c';
+    final apiUrl =
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      List<dynamic> articles = data['articles'];
+
+      setState(() {
+        _newsContent = articles
+            .where((article) =>
+                article['title'] != null &&
+                article['description'] != null &&
+                article['urlToImage'] != null)
+            .map((article) => {
+                  'title': article['title'],
+                  'content': article['description'],
+                  'image': article['urlToImage'],
+                })
+            .toList();
+      });
+    } else {
+      print('Falha na requisição: ${response.statusCode}');
+    }
+  }
 
   void _onTabTapped(int index) {
     setState(() {
@@ -106,106 +114,62 @@ class _NewsHomePageState extends State<NewsHomePage> {
         title: Text('Notícias'),
       ),
       body: Center(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            if (constraints.maxWidth > 600) {
-              // Se a largura da tela for maior que 600 (modo tablet), exibe o layout Row
-              return Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Imagem da notícia à esquerda
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      _newsContent[_currentTabIndex]['image']!,
-                      width: 150,
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  // Texto da notícia à direita
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _newsContent[_currentTabIndex]['title']!,
+        child: _newsContent.isEmpty
+            ? CircularProgressIndicator()
+            : LayoutBuilder(
+                builder: (context, constraints) {
+                  final currentNews = _newsContent[_currentTabIndex];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          currentNews['title'] ?? 'Título não disponível',
                           style: TextStyle(
                             fontSize: 20.0,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(height: 10),
-                        Text(
-                          _newsContent[_currentTabIndex]['content']!,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: currentNews['image'] != null
+                            ? Image.network(
+                                currentNews['image'],
+                                width: double.infinity,
+                                height: 200,
+                                fit: BoxFit.cover,
+                              )
+                            : Placeholder(fallbackHeight: 200),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          currentNews['content'] ?? 'Conteúdo não disponível',
                           style: TextStyle(fontSize: 16.0),
                         ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            } else {
-              // Se a largura da tela for menor que 600 (modo celular), exibe o layout Column
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Imagem da notícia
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      _newsContent[_currentTabIndex]['image']!,
-                      width: 150,
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  // Texto da notícia
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _newsContent[_currentTabIndex]['title']!,
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          _newsContent[_currentTabIndex]['content']!,
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            }
-          },
-        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentTabIndex,
-        onTap: _onTabTapped,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books),
-            label: 'Notícia 1',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books),
-            label: 'Notícia 2',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_books),
-            label: 'Notícia 3',
-          ),
-        ],
-      ),
+      bottomNavigationBar: _newsContent.isEmpty
+          ? null
+          : BottomNavigationBar(
+              currentIndex: _currentTabIndex,
+              onTap: _onTabTapped,
+              items: _newsContent.map((article) {
+                return BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.library_books,
+                    color: Colors.green,
+                  ),
+                  label: article['title'].toString(),
+                );
+              }).toList(),
+            ),
     );
   }
 }
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Oar_Noticias_Dart" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "PROJECT_DIR=D:\\Oar_Noticias_Dart"
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Oar_Noticias_Dart\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Oar_Noticias_Dart"
+  "FLUTTER_TARGET=D:\\Oar_Noticias_Dart\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMmI2MDBmMjQwNTQ4YzFjZjZmNWQzZjlhZTY1YTVjOWU1MWJjNDQzLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Oar_Noticias_Dart\\.dart_tool\\package_config.json"
+)
